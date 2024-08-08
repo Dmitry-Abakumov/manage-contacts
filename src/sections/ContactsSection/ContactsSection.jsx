1d85f98d0b01fcdccbfc596e801bd1ba8aa36948
Dmitry Abakumov <dm.abakumov@gmail.com>
2024-08-09 02:31:01 +0300

    fixes bugs

--- src/sections/ContactsSection/ContactsSection.jsx
+++ src/sections/ContactsSection/ContactsSection.jsx
@@ -5,7 +5,7 @@ import data from "@/data/data.json";
 export const ContactsSection = () => {
   const { contactsSectionTitle } = data;
   return (
-    <section className="py-[22px] md:py-0 md:mt-[-375px]  md:flex md:justify-end">
+    <section className="py-[22px] md:py-0 md:mt-[-387px]  md:flex md:justify-end">
       <div className="smOnly:container">
         <h1 className="ml-[24px] text-[20px] font-[500]">
           {contactsSectionTitle}
